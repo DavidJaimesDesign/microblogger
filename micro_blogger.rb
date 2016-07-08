5b89fccd3b9cd22c34c2ec86af7e320302cc55a7
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-07-07 22:26:50 -0700

    can now spam a follower

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -16,16 +16,33 @@ class MicroBlogger
 		end
 	end
 
+	def dm(target, message)
+		screen_names = @client .followers.collect { |follower| @client.user(follower).screen_name}
+		puts "Trying to send #{target} this direct message"
+		puts message
+		message = "d @#{target} #{message}"
+		screen_names.each do |screen_name|
+			if screen_name == target 
+				tweet(message)
+			end
+		end
+	end
+
 	def run
 		puts "welcome to the DJD twitter client for Spamy McSpamface"
-		case command
-			when 'q' then puts "Client Terminated"
-			else
-				puts "Error command #{command} not recognized...sorry"
-		end
+		command = ""
 		while command != "q"
 			printf "Enter command: "
-			command = gets.chomp
+			input = gets.chomp
+			parts = input.split(" ")
+			command = parts[0]
+			case command
+				when 'q' then puts "Client Terminated"
+				when 't' then tweet(parts[1..-1].join(" "))
+				when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+				else
+					puts "Error command #{command} not recognized...sorry"
+			end
 		end
 	end
 end
